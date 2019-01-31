3b58535c0be67b3e627b62ddc21b174391bb4372
natalynyu <natalyn.yu@gmail.com>
2019-01-31 11:27:09 -0500

    Update app.js for doctors
    Add doctorEvents to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@
 
 const authorizationEvents = require('./authorization/events')
 const reservationEvents = require('./reservations/events')
+const doctorEvents = require('./doctors/events')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -11,6 +12,7 @@ const reservationEvents = require('./reservations/events')
 $(() => {
   authorizationEvents.addAuthorizationHandlers()
   reservationEvents.addReservationHandlers()
+  doctorEvents.addDoctorHandlers()
   $('#sign-out').hide()
   $('#change-password').hide()
   $('#change-pw-heading').hide()
