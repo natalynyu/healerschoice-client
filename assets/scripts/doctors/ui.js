e32e5e7ed6306703f0436b879c34e5e19e2fefa3
natalynyu <natalyn.yu@gmail.com>
2019-01-31 20:16:58 -0500

    Include onCreateDoctorInfo UI
    Add functions for sucessful and failed creations of doctor

--- assets/scripts/doctors/ui.js
+++ assets/scripts/doctors/ui.js
@@ -2,6 +2,19 @@
 
 const store = require('../store')
 
+const onCreateDoctorInfoSuccess = responseData => {
+  console.log(responseData)
+  const doctorFullName = responseData.doctor.full_name
+  $('#doctor-info-message').text('Thanks, ' + doctorFullName + '! Successfully saved your info.').show().fadeOut(3500)
+  $('#create-doctor-info').hide()
+  $('#create-doctor-info-heading').hide()
+  $('#doctorID').val(responseData.doctor.id)
+}
+
+const onCreateDoctorInfoFail = () => {
+  $('#doctor-info-message').text('Error with adding doctor info. Please try again.').show()
+}
+
 const onUpdateDoctorInfoSuccess = () => {
   $('#doctor-info-message').text('Successfully updated info.').show()
 }
@@ -10,6 +23,8 @@ const onUpdateDoctorInfoFail = () => {
   $('#doctor-info-message').text('Error with updating info. Please try again.').show()
 }
 module.exports = {
+  onCreateDoctorInfoSuccess,
+  onCreateDoctorInfoFail,
   onUpdateDoctorInfoSuccess,
   onUpdateDoctorInfoFail
 }
