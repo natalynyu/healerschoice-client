7798e1128146a233141f9947354a3dca9449e93e
natalynyu <natalyn.yu@gmail.com>
2019-02-02 00:20:19 -0500

    Update doctor and reservation UI
    Hide and show doctor and reservation items appropriately

--- assets/scripts/doctors/ui.js
+++ assets/scripts/doctors/ui.js
@@ -3,11 +3,16 @@
 const store = require('../store')
 
 const onCreateDoctorInfoSuccess = responseData => {
-  console.log(responseData)
+  store.doctor = responseData.doctor
   const doctorFullName = responseData.doctor.full_name
+  $('#doctor-info-message').text('')
   $('#doctor-info-message').text('Thanks, ' + doctorFullName + '! Successfully saved your info.').show().fadeOut(3500)
+  $('.reservation-management').show()
+  $('#show-my-reservations').show()
   $('#create-doctor-info').hide()
   $('#create-doctor-info-heading').hide()
+  $('#update-existing-doctor-info').show()
+  $('#welcome-user-back-message').text('')
   $('#doctorID').val(responseData.doctor.id)
 }
 
@@ -15,8 +20,9 @@ const onCreateDoctorInfoFail = () => {
   $('#doctor-info-message').text('Error with adding doctor info. Please try again.').show()
 }
 
-const onUpdateDoctorInfoSuccess = () => {
+const onUpdateDoctorInfoSuccess = responseData => {
   $('#doctor-info-message').text('Successfully updated info.').show()
+  store.doctor = responseData.doctor
 }
 
 const onUpdateDoctorInfoFail = () => {
